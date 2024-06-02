bee523959bd7493b8dbcbe19af9ff0e51223033c
KumPrashant15 <prashant.k0515@gmail.com>
2024-06-02 22:36:16 +0530

    first commit

---
+++ src/Components/Birthday.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Birthday = ({ data }) => {
+  // console.log(data);
+  let apiData = { data };
+  let fav = { data };
+  let favo = { data };
+  // console.log(favo);
+  // console.log(fav.data);
+  // console.log(apiData);
+  // let dataB = apiData.data;
+  // console.log(apiData.data.apiData);
+  let myData = apiData.data.apiData;
+  let { setFavourite } = fav.data;
+  let { favourite } = favo.data;
+  console.log(favourite);
+  // console.log(setFavourite);
+
+  const handlChange = (ele) => {
+    console.log(ele);
+    setFavourite([...favourite, ele]);
+  };
+
+  return (
+    <div className="birthday">
+      <h4>Birthdays on selected date</h4>
+      <div className="container">
+        {myData?.map((ele, idx) => {
+          return (
+            <p key={idx + 1}>
+              {ele}{" "}
+              <button onClick={() => handlChange(ele)}>Add To Favourite</button>
+            </p>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Birthday;
---
+++ src/Components/Calendar.jsx
@@ -0,0 +1,83 @@
+import { LocalizationProvider, StaticDatePicker } from "@mui/x-date-pickers";
+import React, { useEffect, useState } from "react";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
+// import axios from "axios";
+import { format } from "date-fns";
+import Birthday from "./Birthday";
+
+const calendar = () => {
+  const [value, setValue] = useState(new Date());
+  const [apiData, setApiData] = useState([]);
+  const month = format(value, "MM");
+  const day = format(value, "dd");
+
+  const [favourite, setFavourite] = useState([]);
+  //   console.log(value.getDate());
+  // console.log(value.getMonth() + 1);
+
+  // const birthDayData = (month,day) =>{
+  //     let {data} = axios.get(`https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/births/06/01`)
+  //     console.log(data);
+  // }
+
+  // useEffect(()=>{
+  //     birthDayData(month,day);
+  // },[value])
+
+  const handleChange = (date) => {
+    setValue(date);
+  };
+
+  const loadData = (month, day) => {
+    fetch(
+      `https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/births/${month}/${day}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+        const titles = [];
+        data.births.forEach((birth) => {
+          birth.pages.forEach((page) => {
+            // console.log(page.normalizedtitle);
+            // console.log(page.title);
+            titles.push(page.title);
+          });
+        });
+        // console.log(titles);
+        setApiData(titles);
+      });
+  };
+
+  // console.log(apiData);
+
+  // console.log(favourite);
+  useEffect(() => {
+    loadData(month, day);
+  }, [value]);
+
+  //   console.log(monthMap[month]);
+  return (
+    <div className="wrapper">
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <StaticDatePicker
+          className="date"
+          value={value}
+          openTo="day"
+          onChange={handleChange}
+        />
+        {/* <p>Selected Date = {value.getDate()}</p> */}
+      </LocalizationProvider>
+      <Birthday data={{ apiData, favourite, setFavourite }} />
+      <div className="favouriteBirth">
+        <h4>Favourites</h4>
+        <div className="container">
+          {favourite?.map((ele, idx) => {
+            return <p key={idx + 1}>{ele} </p>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default calendar;
